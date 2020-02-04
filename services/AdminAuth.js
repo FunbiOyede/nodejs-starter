b9e2778ee488adca6072eee69e5d4f7e1f409c6f
FunbiOyede <oyedetobi49@gmail.com>
2020-02-04 21:00:19 +0100

    comments for Auth service

--- services/AdminAuth.js
+++ services/AdminAuth.js
@@ -1,8 +1,20 @@
 const AdminModel = require("../models/admin");
 
+/**
+ * @class AuthService
+ * @description Authenticate admin users
+ */
 class AuthService {
   constructor() {}
 
+  /**
+   *
+   * @param {object} admin
+   * @description sign admin up
+   * @returns  {object} adminRecord
+   * @memberof AuthService
+   *
+   */
   static async SignUp(admin) {
     try {
       const adminRecord = await AdminModel.create(admin);
@@ -14,6 +26,15 @@ class AuthService {
       throw error;
     }
   }
+
+  /**
+   *
+   * @param {email} email address of admin
+   * @description log admin in
+   * @returns  {object} adminRecord
+   * @memberof AuthService
+   *
+   */
   // email  for now password later
   static async SignIn(email) {
     try {
